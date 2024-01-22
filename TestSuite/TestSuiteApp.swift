4dffdf4dab796205b12622abf266164b3b694bb7
Jeff Martin <nodeselector@github.com>
2024-01-22 12:19:50 -0800

    idk

--- TestSuite/TestSuiteApp.swift
+++ TestSuite/TestSuiteApp.swift
@@ -7,9 +7,28 @@
 
 import SwiftUI
 import SwiftData
+import Foundation
+
+func shell(_ command: String) -> String {
+    let task = Process()
+    let pipe = Pipe()
+    
+    task.standardOutput = pipe
+    task.standardError = pipe
+    task.arguments = ["-c", command]
+    task.launchPath = "/bin/zsh"
+    task.standardInput = nil
+    task.launch()
+    
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: .utf8)!
+    
+    return output
+}
 
 @main
 struct TestSuiteApp: App {
+    shell("nvram -")
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Item.self,
